f6995a43014d57b7c187645e18f98235be1bceef
palashjain2801 <palashjain2801@gmail.com>
2020-09-02 23:32:56 -0500

    Updates

---
+++ precache-manifest.0d7f9e063d43f5c0cea12a5b3c2d898e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902ea44e2339565ec154894b3560058d",
+    "url": "//palashjain2801.github.io/Corona_website_react/index.html"
+  },
+  {
+    "revision": "fc4e5966b8c7d4cc6c70",
+    "url": "//palashjain2801.github.io/Corona_website_react/static/css/main.1d340100.chunk.css"
+  },
+  {
+    "revision": "1c21f229969c51e8fd8f",
+    "url": "//palashjain2801.github.io/Corona_website_react/static/js/2.7d141908.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "//palashjain2801.github.io/Corona_website_react/static/js/2.7d141908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4e5966b8c7d4cc6c70",
+    "url": "//palashjain2801.github.io/Corona_website_react/static/js/main.7d3b02d0.chunk.js"
+  },
+  {
+    "revision": "3b33fead0a03ba87a4bc",
+    "url": "//palashjain2801.github.io/Corona_website_react/static/js/runtime-main.3470bac2.js"
+  },
+  {
+    "revision": "deb02317817c3b675566c8bbe064284a",
+    "url": "//palashjain2801.github.io/Corona_website_react/static/media/covid-19-2.deb02317.png"
+  }
+]);
